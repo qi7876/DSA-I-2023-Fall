942f5eccec1e322b35392e344cf85e00662676ff
Qiww <qi7876@gmail.com>
2023-09-14 07:58:36 +0800

    Changes

---
+++ 4.cpp
@@ -0,0 +1,1 @@
+// Created by Qi7 on 2023/9/14.
--- 作业2.c
+++ 作业2.c
@@ -3,5 +3,10 @@
 #include <stdio.h>
 int main(void)
 {
-
-}*/
+    int n;
+    scanf("%d", &n);
+    printf("%d %d %d %d %d\n\r", n, n*n, n*n*n, n*n*n*n, n*n*n*n*n);
+    fflush(stdin); // 强制清空缓冲区，便于及时输出
+    return 0;
+}
+*/
