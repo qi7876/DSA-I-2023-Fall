8b2e026a37f752d32d43cc32114384e464330b3e
Qiww <qi7876@gmail.com>
2023-09-19 20:17:56 +0800

    2023/9/19 20:16 from mac

---
+++ Chapter5/5.11.8.c
@@ -0,0 +1,20 @@
+/*
+// Created by Qi7 on 2023/9/19.
+#include "stdio.h"
+
+int main(void)
+{
+    int UserInputModNum, UserInputNum;
+    printf("Please input a mod num.\n");
+    scanf("%d", &UserInputModNum);
+    printf("Please enter the first operand: ");
+    scanf("%d", &UserInputNum);
+    while (UserInputNum > 0)
+    {
+        printf("%d %% %d is %d\n", UserInputNum, UserInputModNum, UserInputNum % UserInputModNum);
+        printf("Enter next number for first operand (<=0 to quit): ");
+        scanf("%d", &UserInputNum);
+    }
+    printf("Done!");
+    return 0;
+}*/
---
+++ Chapter5/5.11.9.c
@@ -0,0 +1,25 @@
+/*
+// Created by Qi7 on 2023/9/19.
+#include "stdio.h"
+
+#define KELVIN 273.16
+void temperatures(double Temp);
+
+int main(void)
+{
+    double UserInputTemp;
+    printf("Please enter(q to quit)\n");
+    while (scanf("%lf", &UserInputTemp) == 1)
+    {
+        temperatures(UserInputTemp);
+        printf("Please enter(q to quit)\n");
+    }
+    return 0;
+}
+
+void temperatures(double Temp)
+{
+    printf("S:%.2lf\n", 5.0 / 9.0 * (Temp - 32.0));
+    printf("K:%.2lf\n", 5.0 / 9.0 * (Temp - 32.0) + 273.16);
+    printf("H:%.2lf\n", Temp);
+}*/
