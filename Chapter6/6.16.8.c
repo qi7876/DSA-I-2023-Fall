0bb39079c52214cc2d2fb660ee2fbb0bd15d4da8
Qi7 <Qi7876@gmail.com>
2023-09-23 19:11:00 +0800

    2023/9/23

--- Chapter6/6.16.8.c
+++
@@ -1,7 +0,0 @@
-// Created by Qi7 on 2023/9/21.
-#include "string.h"
-
-int main(void)
-{
-
-}
---
+++ Homework/Homework2.c
@@ -0,0 +1,20 @@
+/*
+// Created by Qi7 on 2023/9/21.
+#include "stdio.h"
+
+int main(void)
+{
+    int A, B, a, b, c;
+    scanf("%d %d", &A, &B);
+    a = A;
+    b = B;
+    c = A % B;
+    while (c != 0)
+    {
+        A = B;
+        B = c;
+        c = A % B;
+    }
+    printf("%d,%d", B, a * b / B);
+    return 0;
+}*/
